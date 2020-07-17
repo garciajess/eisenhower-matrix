9fd10b7d51b94f52b731f8e87570c99e4bb5e461
Deepti Vaidyanathan <deepti.vaidyanathan1@gmail.com>
2020-07-17 12:07:02 -0500

    Fetches the next 10 calendar events from user's calendar

--- eisenhower-matrix/src/main/webapp/script.js
+++ eisenhower-matrix/src/main/webapp/script.js
@@ -1,15 +1,3 @@
-// check login status on load
-document.addEventListener("DOMContentLoaded", function() {
-    // every time there is a submission, fetch tasks
-    getTasks();
-
-    if (userLoggedIn()) {
-        loginStatus(true);
-    } else {
-        loginStatus(false);
-    }
-});
-
 // fetch login link
 async function login() {
     const response = await fetch("/authenticate");
@@ -47,7 +35,8 @@ async function getTasks() {
 
     const response = await fetch("/add-task");
     const text = await response.text();
-
+    console.log("text: ");
+    console.groupCollapsed(text);
     var all_tasks = text.split("\n");
     all_tasks = all_tasks.filter((x) => x.length > 0);
 
@@ -122,7 +111,138 @@ async function userLoggedIn() {
 
 // fetch calendar data
 async function calendarGetData() {
-    const response = await fetch("/calendar");
-    const text = await response.text();
-    console.log(text);
+    console.log("need to get calendar data");
+
+    var CLIENT_ID = '470404283189-q3gbv28dhmra1bg82g1evcn4c6gt3d2k.apps.googleusercontent.com';
+      var API_KEY = 'AIzaSyAjMcp7ri61JviqGvpy6_uCU4upTsCYyUw';
+
+      // Array of API discovery doc URLs for APIs used by the quickstart
+      var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+      var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
+      var authorizeButton = document.getElementById('login');
+      var signoutButton = document.getElementById('logout');
+    fetch("https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest") // Call the fetch function passing the url of the API as a parameter
+    .then(function() {
+    // Your code for handling the data you get from the API
+        
+        // On load, called to load the auth2 library and API client library.
+            console.log("gonna gapi load");
+            gapi.load('client:auth2', initClient);
+        
+        // Initializes the API client library and sets up sign-in state
+        // listeners.
+        
+        function initClient() {
+            console.log("init Client start");
+            gapi.client.init({
+            apiKey: API_KEY,
+            clientId: CLIENT_ID,
+            discoveryDocs: DISCOVERY_DOCS,
+            scope: SCOPES
+            }).then(function () {
+            // Listen for sign-in state changes.
+            gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+            // Handle the initial sign-in state.
+            updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+            authorizeButton.onclick = handleAuthClick;
+            signoutButton.onclick = handleSignoutClick;
+            }, function(error) {
+            appendPre(JSON.stringify(error, null, 2));
+            });
+        }
+
+        // /**
+        // *  Called when the signed in status changes, to update the UI
+        // *  appropriately. After a sign-in, the API is called.
+        // */
+        function updateSigninStatus(isSignedIn) {
+            console.log("In update signin status");
+            if (isSignedIn) {
+            getTasks();
+            authorizeButton.style.display = 'none';
+            signoutButton.style.display = 'block';
+            document.getElementById("sample-events").classList.remove("hide")
+            document.getElementById("fetched-content").classList.remove("hide")
+            listUpcomingEvents();
+            } else {
+            authorizeButton.style.display = 'block';
+            signoutButton.style.display = 'none';
+            document.getElementById("sample-events").classList.add("hide");
+            document.getElementById("fetched-content").classList.add("hide")
+            }
+        }
+
+        // /**
+        // *  Sign in the user upon button click.
+        // */
+        function handleAuthClick(event) {
+            console.log(gapi.auth2.getAuthInstance().signIn());
+            gapi.auth2.getAuthInstance().signIn();
+            updateSigninStatus(true);
+        }
+
+        // /**
+        // *  Sign out the user upon button click.
+        // */
+        function handleSignoutClick(event) {
+            gapi.auth2.getAuthInstance().signOut();
+            updateSigninStatus(false);
+        }
+
+        // /**
+        // * Append a pre element to the body containing the given message
+        // * as its text node. Used to display the results of the API call.
+        // *
+        // * @param {string} message Text to be placed in pre element.
+        // */
+        function appendPre(message) {
+            var pre = document.getElementById('sample-events');
+            var textContent = document.createTextNode(message + '\n');
+            pre.appendChild(textContent);
+        }
+
+        // /**
+        // * Print the summary and start datetime/date of the next ten events in
+        // * the authorized user's calendar. If no events are found an
+        // * appropriate message is printed.
+        // */
+        function listUpcomingEvents() {
+            console.log(document.getElementById("sample-events").innerText);
+            if (document.getElementById("sample-events").innerText == "") {
+                gapi.client.calendar.events.list({
+            'calendarId': 'primary',
+            'timeMin': (new Date()).toISOString(),
+            'showDeleted': false,
+            'singleEvents': true,
+            'maxResults': 10,
+            'orderBy': 'startTime'
+            }).then(function(response) {
+            var events = response.result.items;
+            appendPre('Upcoming events:\n');
+
+            if (events.length > 0) {
+                for (i = 0; i < events.length; i++) {
+                var event = events[i];
+                var when = event.start.dateTime;
+                if (!when) {
+                    when = event.start.date;
+                }
+                appendPre(event.summary + ' (' + when + ')\n')
+                }
+            } else {
+                appendPre('No upcoming events found.');
+            }
+            });
+            }
+        }
+
+    })
+    .catch(function() {
+        console.error();
+    });
 }
